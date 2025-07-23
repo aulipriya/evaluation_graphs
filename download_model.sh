382c655963737b2dc5af5b593e0df2e3759e981a
aulipriya <aulipriyapromona@gmail.com>
2025-07-23 16:35:25 +0600

    escape directory

--- download_model.sh
+++ download_model.sh
@@ -4,8 +4,4 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # Install gdown if it's not already installed
 pip install --no-cache-dir gdown
 
-# Make a directory
-mkdir -p /app/example_models
-cd /app/example_models
-
 gdown --folder https://drive.google.com/drive/folders/1RWfl_mU-0kAR92kXVZlHvNadIiok90cE
